20d779f0b2e62e479f00f4a2908aca32c54e84fd
akiraandy <andypschwartz@gmail.com>
2018-01-09 14:26:25 -0600

    install react-redux and redux saga. Add playlist.js and muscleList.js

--- src/containers/muscle_list/muscleList.js
+++ src/containers/muscle_list/muscleList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Playlist extends Component {
+export default class MuscleList extends Component {
   render() {
     return (
       <div>
